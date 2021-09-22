b0d4e1fb06b1396701f6dfd8c8f76abfe3e10fd6
NobutakaMuranaka <chanayataka2324@gmail.com>
2021-09-22 05:11:18 +0000

    管理ユーザーの詳細画面の実装

--- app/controllers/admin_users_controller.rb
+++ app/controllers/admin_users_controller.rb
@@ -8,6 +8,24 @@ class AdminUsersController < ApplicationController
   end
 
   def show
+    @admin_user = AdminUser.find(params[:id])
   end
+  
+  def create
+    @admin_user = AdminUser.new(admin_user_params)
+    if @admin_user.save
+      flash[:success] = "登録が完了しました"
+      redirect_to @admin_user
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def admin_user_params
+      params.require(:admin_user).permit(:email, :password,
+                                   :password_confirmation)
+    end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_21_064819) do
+ActiveRecord::Schema.define(version: 2021_09_21_065433) do
 
   create_table "admin_users", force: :cascade do |t|
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
