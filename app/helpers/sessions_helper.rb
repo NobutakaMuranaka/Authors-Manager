7bcf06c8861ec47a9baa09c1110224209ae58df6
NobutakaMuranaka <chanayataka2324@gmail.com>
2021-09-22 05:21:36 +0000

    ログイン機能の実装

---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,20 @@
+module SessionsHelper
+  def log_in(admin_user)
+    session[:admin_user_id] = admin_user.id
+  end
+
+  def current_admin_user
+    if session[:admin_user_id]
+      @current_admin_user ||= AdminUser.find_by(id: session[:admin_user_id])
+    end
+  end
+
+  def logged_in?
+    !current_admin_user.nil?
+  end
+
+  def log_out
+    session.delete(:admin_user_id)
+    @current_admin_user = nil
+  end
+end
