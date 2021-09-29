8b3d4c4f7064bb92b2554f7f8ed9d5369976dcf3
NobutakaMuranaka <chanayataka2324@gmail.com>
2021-09-29 05:22:53 +0000

    著者一覧ページと著者詳細ページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'authors/new'
+  get 'authors/show'
+  get 'authors/index'
   get 'sessions/new'
   get :signup,       to: 'admin_users#new'
   resources :admin_users
+  resources :authors
   get    :login,     to: 'sessions#new'
   post   :login,     to: 'sessions#create'
   delete :logout,    to: 'sessions#destroy'
