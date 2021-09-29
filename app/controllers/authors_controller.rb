c74696b6890800ce8143cfd6469e129c76a8090c
NobutakaMuranaka <chanayataka2324@gmail.com>
2021-09-29 06:01:17 +0000

    著者一覧ページの実装

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,8 +1,11 @@
 class AuthorsController < ApplicationController
+
   def index
+    @authors = Author.all
   end
 
   def show
+    @author = Author.find(params[:id])
   end
 
   def new
@@ -37,5 +40,9 @@ class AuthorsController < ApplicationController
 
     def author_params
       params.require(:author).permit(:name, :email, :introduction)
+    end
+    
+    def author_params_update
+      params.require(:author_params).permit(:name, :email, :introduction, :sex)
     end
 end
