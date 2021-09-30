f095805f64d85ab8e5bd21c4c22f4471c8154e41
NobutakaMuranaka <chanayataka2324@gmail.com>
2021-09-30 14:05:17 +0000

    bookモデル関連の作成、登録画面の実装

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,18 @@
+class BooksController < ApplicationController
+  def index
+  end
+  
+  def show
+  end
+  
+  def new
+    @book = Book.new
+  end
+  
+  def update
+  end
+  
+  def edit
+  end
+  
+end
